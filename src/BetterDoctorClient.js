ce6bf9c874ccd83fb963f8f89c8ec062f8b64652
Brandon Morse <brandon@morsecodemedia.com>
2017-10-10 16:54:04 -0400

    repositioned bd endpoint/key setters

--- src/BetterDoctorClient.js
+++ src/BetterDoctorClient.js
@@ -1,12 +1,11 @@
 'use strict';
 const https = require('https');
+const betterDoctorAPIEndpoint = 'https://api.betterdoctor.com/2016-03-01/';
+const betterDoctorAPIKey = '';
 /**
  * This is a small wrapper client for the BetterDoctor API.
  */
 class BetterDoctorClient {
-
-	const betterDoctorAPIEndpoint = 'https://api.betterdoctor.com/2016-03-01/';
-	const betterDoctorAPIKey = '';
 
 	getDoctors(deviceLocation, userLocation, radius) {
 		betterDoctorAPIEndpoint+'doctors?location='+encodeURI(deviceLocation)+'%2C'+radius+'&user_location'+encodeURI(userLocation)+'skip=0&limit=10&user_key='+betterDoctorAPIKey;
